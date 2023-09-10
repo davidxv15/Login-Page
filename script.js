0e2e3e7f966757d748a2a30a0167cd6c4998e766
davidxv15 <davidxvaz@gmail.com>
2023-09-10 15:31:43 -0700

    drop in animation

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 // SLIDE IN AND OTHER ANIMATIONS on page load
+// script.js
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Get the login card element
+    var loginCard = document.querySelector(".wrapper");
+  
+    var animationDuration = 800; 
+
+    // Function to animate the login card
+    function animateLoginCard(timestamp) {
+      // Calculate the progress of the animation (0 to 1)
+      var progress = Math.min(timestamp / animationDuration, 1); // 1000ms = 1s
+  
+      // Apply the animation to the login card
+      loginCard.style.transform = `translateY(${(1 - progress) * -100}%)`;
+      loginCard.style.opacity = progress;
+  
+      // Continue the animation until it's complete
+      if (progress < 1) {
+        requestAnimationFrame(animateLoginCard);
+      }
+    }
+  
+    // Trigger the animation
+    requestAnimationFrame(animateLoginCard);
+  });
+  
